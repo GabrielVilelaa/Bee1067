5282c13fdee61e6ee663e6ffbc6444f003bbddff
Gabriel <Gabriel@DESKTOP-068EL15>
2024-01-24 21:38:30 -0300

    Adicionar arquivos de projeto.

---
+++ Bee1067/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace bee1067
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, cont;
+            cont = 0;
+            x = int.Parse(Console.ReadLine());
+
+            while (cont <= x)
+            {
+                if (cont % 2 != 0)
+                {
+                    Console.WriteLine(cont);
+                }
+                cont++;
+            }
+        }
+    }
+}
